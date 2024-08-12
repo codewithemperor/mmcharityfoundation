b9a166ccf2a46a5c54849c27a7dfc6f3c55bb89a
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-08-12 20:34:48 +0100

    modified:   include/db.php

--- include/db.php
+++ include/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // or your server name
-$username = "root"; // or your database username
-$password = ""; // or your database password
-$dbname = "mmcharity";
+$servername = "localhost:3306"; // or your server name
+$username = "mmchytli"; // or your database username
+$password = "wAPEt1Igbekq"; // or your database password
+$dbname = "mmchytli_mmcharity";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
